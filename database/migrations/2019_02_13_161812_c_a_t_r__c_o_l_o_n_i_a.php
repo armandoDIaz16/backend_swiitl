cde9df5cb8aaeff117e9093227b4bdd9259c48fe
Leah <Soto>
2019-02-13 12:27:23 -0600

    Se agregan tablas de colonias.

---
+++ database/migrations/2019_02_13_161812_c_a_t_r__c_o_l_o_n_i_a.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CATRCOLONIA extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+     public function up()
+    {
+        Schema::create('CATR_COLONIA', function (Blueprint $table) {
+            
+            /* CLAVES PRIMARIAS */
+            $table->increments('PK_COLONIA');
+            $table->primary('PK_COLONIA');
+
+            /* DATOS GENERALES */
+            $table->string('NOMBRE');
+            
+              /* CLAVES FORANEAS */
+            $table->integer('FK_ASENTAMIENTO');
+            $table->foreign('FK_ASENTAMIENTO')
+                  ->references('PK_ASENTAMIENTO')->on('CAT_ASENTAMIENTO');
+
+            /* DATOS DE AUDITORIA */
+            $table->integer('FK_USUARIO_REGISTRO');
+            $table->dateTime('FECHA_REGISTRO')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->integer('FK_USUARIO_MODIFICACION')->nullable();
+            $table->dateTime('FECHA_MODIFICACION')->nullable();
+            $table->char('BORRADO',1)->default(0);
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2019_02_13_162516_c_a_t_r__c_o_d_i_g_o__p_o_s_t_a_l.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CATRCODIGOPOSTAL extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('CATR_CODIGO_POSTAL', function (Blueprint $table) {
+
+            /* CLAVES PRIMARIAS */
+            $table->integer('NUMERO_CODIGO_POSTAL');
+            $table->primary('NUMERO_CODIGO_POSTAL');
+
+            /* DATOS GENERALES */
+
+            /* CLAVES FORANEAS */
+            $table->integer('FK_CIUDAD');
+            $table->foreign('FK_CIUDAD')
+                  ->references('PK_CIUDAD')->on('CATR_CIUDAD');
+
+            /* DATOS DE AUDITORIA */
+            $table->integer('FK_USUARIO_REGISTRO');
+            $table->dateTime('FECHA_REGISTRO')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->integer('FK_USUARIO_MODIFICACION')->nullable();
+            $table->dateTime('FECHA_MODIFICACION')->nullable();
+            $table->char('BORRADO',1)->default(0);
+        });       
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
