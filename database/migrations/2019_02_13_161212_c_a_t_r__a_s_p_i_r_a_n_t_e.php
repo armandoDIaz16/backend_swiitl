61ce94c3a7ff42376eb6e30c6e377565eeb6fc17
FabricioITL <14240570@itleon.edu.mx>
2019-02-13 11:48:45 -0600

    Se agregan tablas para aspirantes

---
+++ database/migrations/2019_02_13_161212_c_a_t_r__a_s_p_i_r_a_n_t_e.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CATRASPIRANTE extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('CATR_ASPIRANTE', function (Blueprint $table) {
+
+            /* CLAVES PRIMARIAS */
+            $table->increments('PK_NUMERO_PREFICHA');
+            $table->primary('PK_NUMERO_PREFICHA');
+
+            /* DATOS GENERALES */
+            $table->integer('PROMEDIO');
+            $table->date('PERIODO');
+            $table->char('ESTATUS_PAGO',1)->default(0);
+
+            /* CLAVES FORANEAS */
+            $table->integer('FK_BACHILLERATO')->nullable();
+            $table->char('FK_CARRERA_1');
+            $table->char('FK_CARRERA_2');
+            $table->integer('FK_PADRE');
+            $table->foreign('FK_BACHILLERATO')->references('PK_BACHILLERATO')->on('CAT_BACHILLERATO');
+            $table->foreign('FK_CARRERA_1')->references('PK_CARRERA')->on('CAT_CARRERA');
+            $table->foreign('FK_CARRERA_2')->references('PK_CARRERA')->on('CAT_CARRERA');
+            $table->foreign('FK_PADRE')->references('PK_USUARIO')->on('CATR_USUARIO');
+
+            /* DATOS DE AUDITORIA */
+            $table->integer('FK_USUARIO_REGISTRO');
+            $table->dateTime('FECHA_REGISTRO')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->integer('FK_USUARIO_MODIFICACION')->nullable();
+            $table->dateTime('FECHA_MODIFICACION')->nullable();
+            $table->char('BORRADO',1)->default(0);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('CATR_ASPIRANTE');
+    }
+}
