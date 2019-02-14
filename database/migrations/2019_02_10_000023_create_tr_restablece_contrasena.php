d7afa56f8a5e30e8a11554d56aab407561088711
FabricioITL <14240570@itleon.edu.mx>
2019-02-13 18:21:16 -0600

    Migración restablecer contraseña

--- database/migrations/2019_02_10_000023_create_tr_restablece_contrasena.php
+++ database/migrations/2019_02_10_000023_create_tr_restablece_contrasena.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTrRecuperaContrasena extends Migration
+class CreateTrRestableceContrasena extends Migration
 {
     /**
      * Run the migrations.
