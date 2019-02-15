dffe470dfa51ba46139232afefbd6afd4a606e81
EDUARDO <CHAVEZ>
2019-02-15 12:55:08 -0600

    RAMA DE CORREO

--- app/Http/Controllers/DependenciaController.php
+++ app/Http/Controllers/DependenciaController.php
@@ -4,9 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Dependencia;
+use App\Mail\DemoEmail;
+use Illuminate\Support\Facades\Mail;
 
 class DependenciaController extends Controller
 {
+    public function send()
+    {
+        $objDemo = new \stdClass();
+        $objDemo->demo_one = 'Demo One Value';
+        $objDemo->demo_two = 'Demo Two Value';
+        $objDemo->sender = 'SenderUserName';
+        $objDemo->receiver = 'ReceiverUserName';
+
+        Mail::to("eddychavezba@gmail.com")->send(new DemoEmail($objDemo));
+    }
     /**
      * Display a listing of the resource.
      *
@@ -46,6 +58,8 @@ class DependenciaController extends Controller
      */
     public function show($id)
     {
+        Mail::to("14240683@itleon.edu.mx")->send("Hola");
+        //codigo envio correos
         return Dependencia::select('PK_DEPENDENCIA','NOMBRE')->get();
     }
 
