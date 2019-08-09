4d9d488115896763ddb1caf36172deb2adfeb20e
Daniel <14240617@itleon.edu.mx>
2019-08-09 14:14:30 -0500

    envio de correo activacion de cuentas v1

---
+++ resources/views/mails/activacion_cuenta.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h4>Bienvenido a la plataforma TecVirtual del Tecnológico Nacional de México en León</h4>
+<p>Para poder activar tu cuenta da click en el siguiente enlace:</p>
+<br>
+<div class="text-center">
+<a href="http://localhost:4200/#/activa-cuenta?token={{$curp}}">Activa tu cuenta</a>
+</div>
+</body>
+</html>
