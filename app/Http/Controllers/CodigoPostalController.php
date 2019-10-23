05b9a498ea4911be35f79419e7340e3ddefeb124
Miguel Peña <miguelangel.plopez@itleon.edu.mx>
2019-10-23 16:16:43 -0500

    Avance en perfil de usuario

--- app/Http/Controllers/CodigoPostalController.php
+++ app/Http/Controllers/CodigoPostalController.php
@@ -87,58 +87,77 @@ class CodigoPostalController extends Controller
         //
     }
 
-    public function  procesa_codigo_postal(Request $request) {
-        error_log("CP: " . $request->codigo_postal);
-
+    public function procesa_codigo_postal(Request $request) {
         if ($request->codigo_postal) {
             $data = [];
+            $datos_pais = $this->get_datos_pais($request->codigo_postal);
 
-            $sql = "
-                SELECT
-                    CAT_PAIS.PK_PAIS,
-                    CAT_PAIS.NOMBRE AS NOMBRE_PAIS,
-                    CAT_ENTIDAD_FEDERATIVA.PK_ENTIDAD_FEDERATIVA,
-                    CAT_ENTIDAD_FEDERATIVA.NOMBRE AS NOMBRE_ENTIDAD_FEDERATIVA,
-                    CAT_CIUDAD.PK_CIUDAD,
-                    CAT_CIUDAD.NOMBRE AS NOMBRE_CIUDAD,
-                    CAT_CODIGO_POSTAL.PK_CODIGO_POSTAL,
-                    CAT_CODIGO_POSTAL.NUMERO AS CODIGO_POSTAL
-                FROM
-                    CAT_CODIGO_POSTAL
-                    LEFT JOIN TR_COLONIA_CODIGO_POSTAL
-                        ON CAT_CODIGO_POSTAL.PK_CODIGO_POSTAL = TR_COLONIA_CODIGO_POSTAL.FK_CODIGO_POSTAL
-                    LEFT JOIN CAT_COLONIA
-                        ON TR_COLONIA_CODIGO_POSTAL.FK_COLONIA = CAT_COLONIA.PK_COLONIA
-                    LEFT JOIN CAT_TIPO_ASENTAMIENTO
-                        ON CAT_COLONIA.FK_TIPO_ASENTAMIENTO = CAT_TIPO_ASENTAMIENTO.PK_TIPO_ASENTAMIENTO
-                    LEFT JOIN CAT_CIUDAD
-                        ON CAT_CODIGO_POSTAL.FK_CIUDAD = CAT_CIUDAD.PK_CIUDAD
-                    LEFT JOIN CAT_ENTIDAD_FEDERATIVA
-                        ON CAT_CIUDAD.FK_ENTIDAD_FEDERATIVA = CAT_ENTIDAD_FEDERATIVA.PK_ENTIDAD_FEDERATIVA
-                    LEFT JOIN CAT_PAIS ON CAT_ENTIDAD_FEDERATIVA.FK_PAIS = CAT_PAIS.PK_PAIS
-                WHERE
-                    CAT_CODIGO_POSTAL.NUMERO = '".$request->codigo_postal."'
-            ;";
-
-            $datos = DB::select($sql);
-
-            error_log(print_r($datos, true));
-            error_log($sql);
-
-            if ($datos) {
-                $data['PK_PAIS']                   = $datos[0]->PK_PAIS;
-                $data['NOMBRE_PAIS']               = $datos[0]->NOMBRE_PAIS;
-                $data['PK_ENTIDAD_FEDERATIVA']     = $datos[0]->PK_ENTIDAD_FEDERATIVA;
-                $data['NOMBRE_ENTIDAD_FEDERATIVA'] = $datos[0]->NOMBRE_ENTIDAD_FEDERATIVA;
-                $data['PK_CIUDAD']                 = $datos[0]->PK_CIUDAD;
-                $data['NOMBRE_CIUDAD']             = $datos[0]->NOMBRE_CIUDAD;
-                $data['PK_CODIGO_POSTAL']          = $datos[0]->PK_CODIGO_POSTAL;
-                $data['CODIGO_POSTAL']             = $datos[0]->CODIGO_POSTAL;
+            if ($datos_pais) {
+                $data['PK_PAIS']                   = $datos_pais[0]->PK_PAIS;
+                $data['NOMBRE_PAIS']               = $datos_pais[0]->NOMBRE_PAIS;
+                $data['PK_ENTIDAD_FEDERATIVA']     = $datos_pais[0]->PK_ENTIDAD_FEDERATIVA;
+                $data['NOMBRE_ENTIDAD_FEDERATIVA'] = $datos_pais[0]->NOMBRE_ENTIDAD_FEDERATIVA;
+                $data['PK_CIUDAD']                 = $datos_pais[0]->PK_CIUDAD;
+                $data['NOMBRE_CIUDAD']             = $datos_pais[0]->NOMBRE_CIUDAD;
+                $data['PK_CODIGO_POSTAL']          = $datos_pais[0]->PK_CODIGO_POSTAL;
+                $data['CODIGO_POSTAL']             = $datos_pais[0]->CODIGO_POSTAL;
+                $data['COLONIAS']                  = $this->get_colonias_codigo_postal($request->codigo_postal);
             }
 
             return response()->json(['data' => $data], Response::HTTP_OK);
         } else {
             return response()->json(['error' => 'No existe el código postal'], Response::HTTP_NOT_FOUND);
         }
+    }
+
+    private function get_colonias_codigo_postal($codigo_postal) {
+        $sql = "
+            SELECT
+                CAT_COLONIA.PK_COLONIA,
+                CAT_COLONIA.NOMBRE,
+                CAT_COLONIA.ALIAS,
+                CAT_COLONIA.ABREVIATURA
+            FROM
+                CAT_COLONIA
+                LEFT JOIN TR_COLONIA_CODIGO_POSTAL
+                    ON TR_COLONIA_CODIGO_POSTAL.FK_COLONIA = CAT_COLONIA.PK_COLONIA
+                LEFT JOIN CAT_CODIGO_POSTAL
+                    ON TR_COLONIA_CODIGO_POSTAL.FK_CODIGO_POSTAL = CAT_CODIGO_POSTAL.PK_CODIGO_POSTAL
+            WHERE
+                CAT_CODIGO_POSTAL.NUMERO = '".$codigo_postal."'
+            ;";
+
+        return DB::select($sql);
+    }
+
+    private function get_datos_pais($codigo_postal) {
+        $sql = "
+            SELECT
+                CAT_PAIS.PK_PAIS,
+                CAT_PAIS.NOMBRE AS NOMBRE_PAIS,
+                CAT_ENTIDAD_FEDERATIVA.PK_ENTIDAD_FEDERATIVA,
+                CAT_ENTIDAD_FEDERATIVA.NOMBRE AS NOMBRE_ENTIDAD_FEDERATIVA,
+                CAT_CIUDAD.PK_CIUDAD,
+                CAT_CIUDAD.NOMBRE AS NOMBRE_CIUDAD,
+                CAT_CODIGO_POSTAL.PK_CODIGO_POSTAL,
+                CAT_CODIGO_POSTAL.NUMERO AS CODIGO_POSTAL
+            FROM
+                CAT_CODIGO_POSTAL
+                LEFT JOIN TR_COLONIA_CODIGO_POSTAL
+                    ON CAT_CODIGO_POSTAL.PK_CODIGO_POSTAL = TR_COLONIA_CODIGO_POSTAL.FK_CODIGO_POSTAL
+                LEFT JOIN CAT_COLONIA
+                    ON TR_COLONIA_CODIGO_POSTAL.FK_COLONIA = CAT_COLONIA.PK_COLONIA
+                LEFT JOIN CAT_TIPO_ASENTAMIENTO
+                    ON CAT_COLONIA.FK_TIPO_ASENTAMIENTO = CAT_TIPO_ASENTAMIENTO.PK_TIPO_ASENTAMIENTO
+                LEFT JOIN CAT_CIUDAD
+                    ON CAT_CODIGO_POSTAL.FK_CIUDAD = CAT_CIUDAD.PK_CIUDAD
+                LEFT JOIN CAT_ENTIDAD_FEDERATIVA
+                    ON CAT_CIUDAD.FK_ENTIDAD_FEDERATIVA = CAT_ENTIDAD_FEDERATIVA.PK_ENTIDAD_FEDERATIVA
+                LEFT JOIN CAT_PAIS ON CAT_ENTIDAD_FEDERATIVA.FK_PAIS = CAT_PAIS.PK_PAIS
+            WHERE
+                CAT_CODIGO_POSTAL.NUMERO = '".$codigo_postal."'
+            ;";
+
+        return DB::select($sql);
     }
 }
